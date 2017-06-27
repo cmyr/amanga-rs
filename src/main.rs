c56b745ae62e9c3ceb64abeec87f1258d93c0e61
Colin Rofls <colin@cmyr.net>
2017-06-27 14:57:50 -0400

    Wrapper for multiple stream partitions

--- src/main.rs
+++ src/main.rs
@@ -33,25 +33,68 @@ fn load_cred(path: &str) -> Credential {
     serde_json::from_slice(&buf).unwrap()
 }
 
-fn start_partition_thread(cred: &Credential, url: &str, chan: mpsc::Sender<String>) {
-    let client = Client::new().unwrap();
-    let stream = client.get(url)
-        .basic_auth(cred.user.clone(), Some(cred.pw.clone()))
-        .header(Accept::json())
-        .header(Connection::keep_alive())
-        .header(AcceptEncoding(vec![qitem(Encoding::Gzip)]))
-        .send()
-        .unwrap();
+struct GnipStream<'a> {
+    base_url: &'a str,
+    parts: usize,
+    running: bool,
+    pub recv: mpsc::Receiver<String>,
+    send: mpsc::Sender<String>,
+    handles: Vec<thread::JoinHandle<()>>,
+}
 
-    println!("{}, {}", stream.status(), stream.headers());
-
-    thread::spawn(move || {
-        let mut reader = BufReader::new(stream);
-        let mut streamer = JsonStreamer::new(&mut reader);
-        while let Some(s) = streamer.next() {
-            chan.send(s).unwrap();
+impl<'a> GnipStream<'a> {
+    pub fn new(base_url: &'a str, parts: usize) -> GnipStream<'a> {
+        let (send, recv) = mpsc::channel();
+        GnipStream {
+            base_url: base_url,
+            parts: parts,
+            running: false,
+            recv: recv,
+            send: send,
+            handles: Vec::new(),
         }
-    });
+    }
+
+    pub fn run(&mut self, cred: &Credential) {
+        if self.running {
+            eprintln!("stream is already running");
+            return
+        }
+
+        for part in 1..self.parts + 1 {
+            let url = format!("{}?partition={}", self.base_url, part);
+            eprintln!("connecting to url {}", url);
+
+            let client = Client::new().unwrap();
+            let stream = client.get(&url)
+                .basic_auth(cred.user.clone(), Some(cred.pw.clone()))
+                .header(Accept::json())
+                .header(Connection::keep_alive())
+                .header(AcceptEncoding(vec![qitem(Encoding::Gzip)]))
+                .send()
+                .unwrap();
+
+            eprintln!("{}, {}", stream.status(), stream.headers());
+
+            let chan = self.send.clone();
+            let t = thread::spawn(move || {
+                let mut reader = BufReader::new(stream);
+                let mut streamer = JsonStreamer::new(&mut reader);
+                while let Some(s) = streamer.next() {
+                    match chan.send(s) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            eprintln!("partition {} channel closed: {:?}", part, e);
+                            break
+                        }
+                    }
+                }
+                //TODO: handle this exit
+            });
+            self.handles.push(t);
+        }
+        self.running = true;
+    }
 }
 
 fn main() {
@@ -61,17 +104,15 @@ fn main() {
     };
 
     let cred = load_cred(&cred_path);
-    let url = "https://gnip-stream.twitter.com/stream/sample10/accounts/anagramatron/publishers/twitter/prod.json?partition=1";
-    let url2 = "https://gnip-stream.twitter.com/stream/sample10/accounts/anagramatron/publishers/twitter/prod.json?partition=2";
-    let (send, recv) = mpsc::channel();
-    start_partition_thread(&cred, &url, send.clone());
-    start_partition_thread(&cred, &url2, send.clone());
+    let url = "https://gnip-stream.twitter.com/stream/sample10/accounts/anagramatron/publishers/twitter/prod.json";
 
-    
+    let mut streamer = GnipStream::new(url, 2);
+    streamer.run(&cred);
+
     let mut count = 0usize;
     let start = Instant::now();
 
-    while let Ok(string) = recv.recv() {
+    while let Ok(string) = streamer.recv.recv() {
         match serde_json::from_str::<TweetSummary>(&string) {
             Ok(t) => println!("{}: {}", t.lang, t.text),
             Err(e) => println!("ERROR {:?}\n{}", e, string),
@@ -102,6 +143,7 @@ impl <'a, R>Iterator for JsonStreamer<'a, R> where R: BufRead + 'a {
 
     fn next(&mut self) -> Option<String> {
         let mut buf = Vec::new();
+        //TODO: handle these two errors
         let _ = self.reader.read_until(b'\r', &mut buf).unwrap();
         String::from_utf8(buf).ok()
     }
