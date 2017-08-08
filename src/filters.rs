cd88bf34de46838b3469a731073863fa6a94794b
Colin Rofls <colin@cmyr.net>
2017-08-08 12:26:19 -0400

    Improve basic filters

--- src/filters.rs
+++ src/filters.rs
@@ -1,4 +1,4 @@
-use super::Tweet;
+use gnip_twitter_stream::Tweet;
 
 type Filter = fn(&Tweet) -> bool;
 
@@ -24,19 +24,41 @@ pub fn letterish(tweet: &Tweet) -> bool {
     let mut letter_chars = 0;
     for chr in tweet.text.chars() {
         total_chars += 1;
+        if is_ascii_letter(&chr) { letter_chars += 1 }
         // ascii letters + space
-        match u32::from(chr) {
-            32 | 65 ... 91 | 97 ... 123 => letter_chars += 1,
-            _ => (),
-        }
     }
-    letter_chars as f64 / total_chars as f64 >= 0.7
+    letter_chars as f64 / total_chars as f64 >= 0.65
+}
+
+pub fn is_ascii_letter(chr: &char) -> bool {
+    match *chr {
+        'a' ... 'z' | 'A' ... 'Z' => true,
+        _ => false,
+    }
 }
 
 pub fn filter_all(tweet: &Tweet) -> bool {
     mention_filter(tweet) &&
-    url_filter(tweet) &&
-    en_filter(tweet) &&
+        url_filter(tweet) &&
+        en_filter(tweet) &&
     manual_url_filter(tweet) &&
     letterish(tweet)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ascii_letter() {
+        assert!(!is_ascii_letter('@'));
+        assert!(!is_ascii_letter('['));
+        assert!(!is_ascii_letter('`'));
+        assert!(!is_ascii_letter('{'));
+        assert!(is_ascii_letter('A'));
+        assert!(is_ascii_letter('Z'));
+        assert!(is_ascii_letter('a'));
+        assert!(is_ascii_letter('z'));
+    }
 }
